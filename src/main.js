426a991857f9671cd583373a4fdefd685236e1f9
Jeremy Kale Padot <jeremy@padot.us>
2020-03-01 22:14:05 -0800

    fix 'undefined' error. undefined is now replaced with no results

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DoctorWho } from './../apicall-service.js';
 
-
 $(document).ready(function() {
   $('#searchDoctor').submit(function(event) {
     event.preventDefault();
@@ -16,9 +15,10 @@ $(document).ready(function() {
         let doctorWho = new DoctorWho();
         if (name === '') {
             $('#output').text("No results are available, make a correct input.");
-        } else {
+    
+        } 
+        else {
 
-        
           console.log(name);
           const response = await doctorWho.getDoctor(name);
           getElements(response);
@@ -27,9 +27,9 @@ $(document).ready(function() {
       
     function getElements(response) {
       console.log(response);
-      if(response == false){
+      if(response == false) {
         $('#output').text("BetterDoctor API error, please try again or request a new key from your developer");
-      }
+        }
       if(response.data && response.data.length > 0) {
         for(let i = 0; i < 10; i++) {
           let doctorFirstName = `${response.data[i].profile.first_name}`;
@@ -39,13 +39,19 @@ $(document).ready(function() {
           let doctorNumber = `${response.data[i].practices[0].phones[0].number}`;
           let doctorSpecialty = `${response.data[i].specialties[0].name}`;
           let doctorProfile = "<br>" + "First name: " + doctorFirstName + "<br>" + "Last name: " + doctorLastName + "<br>" + "Accepting new patients: " + doctorPatients + "<br>" + "Website: " + doctorWebsite + "<br>" + "Phone: " + doctorNumber + "<br>" + "Specialty " + doctorSpecialty + "<br>";
-         
+          
+         if(doctorProfile.includes("undefined")){
+            doctorProfile = doctorProfile.replace("undefined", "No results available");
+          }
           
           $('#output').append(doctorProfile);
         } 
+        
       } else {
-         $('#output').text("No results are available, make a correct input.");
+         $('#output').text("No results are available, please make a correct input.");
         }
+        
     }
   });
 });
+
